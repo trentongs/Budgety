5156b5b6850bdbacf1c04aa15a6ae2afbe3ab4f7
Trenton Samuel <45015901+trentongs@users.noreply.github.com>
2020-10-12 19:25:24 -0700

    Updated app.js to fix some minor problems
    Used parseFloat() method to convert field inputs to numbers.
    Added an if statement to prevent false inputs

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -149,6 +149,14 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
 
+    var updateBudget = function() {
+        // 1. Calculate the budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+
+    };
 
     var ctrlAddItem = function() {
         var input, newItem;
@@ -156,18 +164,19 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 1. Get the field input data
         input = UICtrl.getInput();
 
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        // 4. Clear the fields
-        UICtrl.clearFields();
+            // 4. Clear the fields
+            UICtrl.clearFields();
 
-        // 5. Calculate the budget
-
-        // 6. Display the budget on the UI
+            // 5. Calculate and update budget
+            updateBudget();
+        }
 
     };
 
