1615af08833a01d6f11e64d28f2c149ab55ae558
Trenton Samuel <45015901+trentongs@users.noreply.github.com>
2020-10-15 18:34:08 -0700

    Updated app.js to delete item from data structure
    Created and defined deleteItem function which creates an array and then retrieves index of element that the user wants to remove.
    Fixed bug in ctrlDeleteItem function, in which ID was originally set to a string, so used parseInt() method to convert it to an integer.

--- app.js
+++ app.js
@@ -64,6 +64,26 @@ var budgetController = (function() {
             return newItem;
         },
 
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            // id = 6
+            //data.allItems[type][id];
+            // ids = [1 2 4 6 8]  //in this example, elements with id = 0, 3, 5, 7 were all deleted already
+            //index = 3
+
+            var ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1); // Will remove 1 element
+            }
+
+        },
+
         calculateBudget: function() {
 
             // Calculate total income and expenses
@@ -251,9 +271,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             //inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. Delete the item from the data structure
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
 
