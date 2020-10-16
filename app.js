2e5d3c2df868f518640b515f425d7a6d006c6bda
Trenton Samuel <45015901+trentongs@users.noreply.github.com>
2020-10-15 21:40:44 -0700

    Updated app.js to delete item from UI
    Utilized more DOM manipulation, in which created and defined deleteListItem function to remove an element from the DOM.

--- app.js
+++ app.js
@@ -170,6 +170,13 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        deleteListItem: function(selectorID) {
+
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+
+        },
+
         clearFields: function() {
             var fields, fieldsArr;
 
@@ -277,9 +284,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
+            UICtrl.deleteListItem(itemID);
 
             // 3. Update and show the new budget
-
+            updateBudget();
         }
 
     };
